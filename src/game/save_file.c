678444ce39a1874cf7ab6f82d5f46acc87564d7c
GateGuy <57763469+GateGuy@users.noreply.github.com>
2020-07-16 01:16:03 -0400

    Fixed incompatible for loop declarations

--- src/game/save_file.c
+++ src/game/save_file.c
@@ -183,8 +183,9 @@ static void wipe_main_menu_data(void) {
     gSaveBuffer.menuData[0].coinScoreAges[1] = 0x2AAAAAAA;
     gSaveBuffer.menuData[0].coinScoreAges[2] = 0x15555555;
 
+    u8 i;
     // Set all time trial times to the max amount
-    for (u8 i = 0; i < 118; i++)
+    for (i = 0; i < 118; i++)
         gSaveBuffer.menuData[0].timeTrialTimes[i] = 17999;
     gSaveBuffer.menuData[0].timeTrialTotalTime = 17999 * 118;
 
@@ -690,7 +691,8 @@ void time_trial_save_file_set_time(s32 courseIndex, s16 starIndex, u16 time, u8 
 
 void time_trial_update_total_time(u8 forceSave) {
     gSaveBuffer.menuData[0].timeTrialTotalTime = 0;
-    for (u8 i = 0; i < 118; i++) {
+    u8 i;
+    for (i = 0; i < 118; i++) {
         gSaveBuffer.menuData[0].timeTrialTotalTime += gSaveBuffer.menuData[0].timeTrialTimes[i];
     }
     gMainMenuDataModified = TRUE;
@@ -742,9 +744,11 @@ u32 time_trial_save_file_get_total_time(void) {
 
 // If any times are equal to 0 (which is how they're initialized) or above the max allowed value, reset all times
 void time_trial_verify_times(void) {
-    for (u8 i = 0; i < 118; i++) {
+    u8 i;
+    for (i = 0; i < 118; i++) {
         if (gSaveBuffer.menuData[0].timeTrialTimes[i] == 0 || gSaveBuffer.menuData[0].timeTrialTimes[i] > 17999) {
-            for (u8 j = 0; j < 118; j++) {
+            u8 j;
+            for (j = 0; j < 118; j++) {
                 gSaveBuffer.menuData[0].timeTrialTimes[j] = 17999;
                 time_trial_update_total_time(0);
             }
