567b3a5152c97f07d80ea7e668494f8519162df2
GateGuy <57763469+GateGuy@users.noreply.github.com>
2020-07-20 18:26:22 -0400

    Added verify

--- src/game/save_file.c
+++ src/game/save_file.c
@@ -645,9 +645,25 @@ s32 check_warp_checkpoint(struct WarpNode *warpNode) {
     return isWarpCheckpointActive;
 }
 
+// If any times are equal to 0 (which is how they're initialized) or above the max allowed value, reset all times
+void time_trial_verify_times(void) {
+    u8 i;
+    for (i = 0; i < 118; i++) {
+        if (gSaveBuffer.menuData[0].timeTrialTimes[i] == 0 || gSaveBuffer.menuData[0].timeTrialTimes[i] > 17999) {
+            u8 j;
+            for (j = 0; j < 118; j++) {
+                gSaveBuffer.menuData[0].timeTrialTimes[j] = 17999;
+                time_trial_update_total_time(0);
+            }
+            break;
+        }
+    }
+}
+
 void time_trial_save_file_set_time(s32 courseIndex, s16 starIndex, u16 time, u8 forceSet) {
     u8 timeIndex;
     u8 forceSave = 0;
+    time_trial_verify_times();
     switch (courseIndex) {
         case 15:
             timeIndex = 112 + (3*starIndex);
@@ -741,19 +757,4 @@ u16 time_trial_save_file_get_time(s32 courseIndex, s16 starIndex) {
 
 u32 time_trial_save_file_get_total_time(void) {
     return gSaveBuffer.menuData[0].timeTrialTotalTime;
-}
-
-// If any times are equal to 0 (which is how they're initialized) or above the max allowed value, reset all times
-void time_trial_verify_times(void) {
-    u8 i;
-    for (i = 0; i < 118; i++) {
-        if (gSaveBuffer.menuData[0].timeTrialTimes[i] == 0 || gSaveBuffer.menuData[0].timeTrialTimes[i] > 17999) {
-            u8 j;
-            for (j = 0; j < 118; j++) {
-                gSaveBuffer.menuData[0].timeTrialTimes[j] = 17999;
-                time_trial_update_total_time(0);
-            }
-            break;
-        }
-    }
 }
